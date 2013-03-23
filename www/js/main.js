7e53b4ed99e5b6156004ce13afc3ce41fc3b0b12
Andy Sharman <andy@rdrkt.com>
2013-03-24 05:11:40 +1100

    * isotope working and list interlinked.

--- www/js/main.js
+++ www/js/main.js
@@ -5,15 +5,27 @@ var viewManager = (function () {
 
     _this.run = function () {
 
+        _this.domFix();
+
         _this.loadEvents();
 
         return _this;
     };
 
+    _this.domFix = function () {
+        $('main').isotope();
+    };
+
     _this.loadEvents = function () {
 
         $(document).on('click', '.block', function () {
-            $(this).toggleClass('large-block');
+            $(this).animate({
+                'height': 300,
+                'width': 300
+            }, 100, function () {
+                
+                $(this).toggleClass('large-block');
+            });
         });
 
     };
