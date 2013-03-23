625ff2c20dc483c0d6c90e740f9197a7a31446e5
rdrkt <josh@rdrkt.com>
2013-03-23 20:46:36 +1100

    Flesh out API to allow listing of users and profiles

--- admin/app/Controller/ApiController.php
+++ admin/app/Controller/ApiController.php
@@ -13,11 +13,24 @@ class ApiController extends AppController {
 	
 	public function profile( $id = "" ){
 		if($this->request->isGet()){
+			if(empty($id)){
+				//id is empty so list all profiles
+				
+				$ret = array();
+				$profiles = $this->Profile->find('all');
+
+				foreach($profiles as $profile){
+					$ret[] = $profile['Profile'];
+				}
+				
+				return $this->_toJson($ret);
+			}
+			
 			$profile = $this->Profile->findById($id);
 			
 			$ret = false;
 			if(!empty($profile)){
-				$ret = $this->_toJson($profile['Profile']);
+				$ret = $profile['Profile'];
 			} 
 			
 			return $this->_toJson($ret);
@@ -31,7 +44,40 @@ class ApiController extends AppController {
 		return $this->_toJson(false);
 	}
 	
-	public function block(){
+	public function user( $id = "" ){
+		if(empty($id)){
+			//id is empty so list all users
+		
+			$ret = array();
+			$users = $this->User->find('all');
+		
+			foreach($users as $user){
+				$ret[] = $user['User'];
+			}
+		
+			return $this->_toJson($ret);
+		}
+		
+		if($this->request->isGet()){
+			$user = $this->User->findById($id);
+				
+			$ret = false;
+			if(!empty($user)){
+				$ret = $user['User'];
+			}
+				
+			return $this->_toJson($ret);
+		}
+	
+		if($this->request->isPost()){
+			$ret = false;
+			return $this->_toJson($ret);
+		}
+	
+		return $this->_toJson(false);
+	}
+	
+	public function block( $id = '' ){
 		if($this->request->isGet()){
 			$blocks = $this->Block->find('all');
 			
