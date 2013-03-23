b0d470d33efcd0844a6b69eb51aa782b6683572c
rdrkt <josh@rdrkt.com>
2013-03-23 19:11:38 +1100

    Use mongodb to generate results instead of the stub

--- admin/app/Controller/ApiController.php
+++ admin/app/Controller/ApiController.php
@@ -3,38 +3,23 @@
 class ApiController extends AppController {
 	public $uses = array('Block', 'Users', 'Profiles', 'Logs');
 	
+	protected function _toJsonP($array){
+		return "callback('" . json_encode($array) . "')";		
+	}	
+	
 	public function block(){
 		$this->autoRender = false;
 		
-		#debug( $this->Block->find() );
+		$blocks = $this->Block->find('all');
 		
-		return "callback(" . json_encode(
-			array(
-				'counting' => array(
-					array(
-						'_id' => "countingMongodb",
-						'type'=>"counting",
-						'title'=>"Mongodb Collection Counting",
-						'description'=>"Block for visualizing the size of collections and their indexes",
-						'ttl'=>1000,
-						'options'=>array( 
-							'value1'=>'default',
-							'value2'=>'default2',
-						)
-					),	
-					array(
-						'_id' => "countingBeanstalkd",
-						'type'=>"counting",
-						'title'=>"Beanstalkd Tube Counting",
-						'description'=>"Block for visualizing the size of tubes and the workers that are watching them",
-						'ttl'=>1000,
-						'options'=>array(
-								'value1'=>'default',
-								'value2'=>'default2',
-						)
-					),
-				),
-			)
-		) . ")";
-	}	
+		$result = array();
+		
+		foreach ($blocks as $block){
+			$block = $block['Block'];
+			
+			$result[$block['type']][] = $block;
+		}
+		
+		return $this->_toJsonP($result);
+	}
 }
